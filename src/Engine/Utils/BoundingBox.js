7bc077f9221c2585dd6c260cc8160af856b100a7
LAPTOP-GJ5AE074\yllud <zzxx9633@gmail.com>
2021-11-12 23:45:56 +0900

    utils commit

--- src/Engine/Utils/BoundingBox.js
+++ src/Engine/Utils/BoundingBox.js
@@ -1,32 +1,12 @@
-/* 
- * File: BoundingBox.js
- * Encapsulates an axis aligned box
- */
-
-/*jslint node: true, vars: true, bitwise: true */
-/*global vec2 */
-/* find out more about jslint: http://www.jslint.com/help.html */
 "use strict";
 
-/**
- * Default Constructor
- * Encapsulates an axis aligned box
- * @memberOf BoundingBox
- * @param {vec2} centerPos Center postion of the bounding box
- * @param {Number} w Width of the bounding box
- * @param {Number} h Height of the bounding box
- * @returns {BoundingBox} New instance of BoundingBox
- */
+
 function BoundingBox(centerPos, w, h) {
     this.mLL = vec2.fromValues(0, 0);
     this.setBounds(centerPos, w, h);
 }
 
-/**
- * Collision Status values
- * @memberOf BoundingBox
- * @type {enum|eboundCollideStatus}
- */
+
 BoundingBox.eboundCollideStatus = Object.freeze({
     eCollideLeft: 1,
     eCollideRight: 2,
@@ -35,14 +15,7 @@ BoundingBox.eboundCollideStatus = Object.freeze({
     eInside : 16,
     eOutside: 0
 });
-/**
- * Set the BoundingBox Location and Size. rotation is ignored.
- * @memberOf BoundingBox
- * @param {vec2} centerPos Center postion of the bounding box
- * @param {Number} w Width of the bounding box
- * @param {Number} h Height of the bounding box
- * @returns {void}
- */
+
 BoundingBox.prototype.setBounds = function (centerPos, w, h) {
     this.mWidth = w;
     this.mHeight = h;
@@ -50,24 +23,13 @@ BoundingBox.prototype.setBounds = function (centerPos, w, h) {
     this.mLL[1] = centerPos[1] - (h / 2);
 };
 
-/**
- * Determines if x,y point is inside BoundingBox
- * @memberOf BoundingBox
- * @param {type} x X location to check
- * @param {type} y Y location to check
- * @returns {Boolean} true if x,y point is inside this BoundingBox
- */
+
 BoundingBox.prototype.containsPoint = function (x, y) {
     return ((x > this.minX()) && (x < this.maxX()) &&
              (y > this.minY()) && (y < this.maxY()));
 };
 
-/**
- * Determines if parameter BoundingBox intersects with this BoundingBox
- * @memberOf BoundingBox
- * @param {BoundingBox} otherBound to check intersect status
- * @returns {Boolean} true if paramater intersects this BoundingBox
- */
+
 BoundingBox.prototype.intersectsBound = function (otherBound) {
     return ((this.minX() < otherBound.maxX()) &&
             (this.maxX() > otherBound.minX()) &&
@@ -75,13 +37,7 @@ BoundingBox.prototype.intersectsBound = function (otherBound) {
             (this.maxY() > otherBound.minY()));
 };
 
-/**
- * Return the collision status of parameter BoundingBox
- * @memberOf BoundingBox
- * @param {BoundingBox} otherBound to check collision status
- * @returns {eboundCollideStatus} collision status
- */
-// returns the status of otherBound wrt to this.
+
 BoundingBox.prototype.boundCollideStatus = function (otherBound) {
     var status = BoundingBox.eboundCollideStatus.eOutside;
 
@@ -108,31 +64,8 @@ BoundingBox.prototype.boundCollideStatus = function (otherBound) {
     return status;
 };
 
-/**
- * Return the left position of the BoundingBox
- * @memberOf BoundingBox
- * @returns {Number} left position
- */
-BoundingBox.prototype.minX = function () { return this.mLL[0]; };
 
-/**
- * Return the right position of the BoundingBox
- * @memberOf BoundingBox
- * @returns {Number} right position
- */
+BoundingBox.prototype.minX = function () { return this.mLL[0]; }
 BoundingBox.prototype.maxX = function () { return this.mLL[0] + this.mWidth; };
-
-/**
- * Return the bottom position of the BoundingBox
- * @memberOf BoundingBox
- * @returns {Number} bottom position
- */
 BoundingBox.prototype.minY = function () { return this.mLL[1]; };
-
-/**
- * Return the top position of the BoundingBox
- * @memberOf BoundingBox
- * @returns {Number} top position
- */
 BoundingBox.prototype.maxY = function () { return this.mLL[1] + this.mHeight; };
-//</editor-fold>
--- src/Engine/Utils/CollisionInfo.js
+++ src/Engine/Utils/CollisionInfo.js
@@ -1,13 +1,3 @@
-/* 
- * File: CollisionInfo.js
- *      normal: vector upon which collision interpenetrates
- *      depth: how much penetration
- */
-
-/*jslint node: true, vars: true, white: true */
-/*global vec2 */
-/* find out more about jslint: http://www.jslint.com/help.html */
-
 "use strict"; 
 
 function CollisionInfo() {
@@ -17,6 +7,5 @@ function CollisionInfo() {
 
 CollisionInfo.prototype.setDepth = function (s) { this.mDepth = s; };
 CollisionInfo.prototype.setNormal = function (s) { this.mNormal = s; };
-
 CollisionInfo.prototype.getDepth = function () { return this.mDepth; };
 CollisionInfo.prototype.getNormal = function () { return this.mNormal; };
