a6096fbc54032c05ff58968049d977a130322f69
SUPERKEON\keon <up2sky7@naver.com>
2021-11-12 23:49:56 +0900

    Renderable

--- src/Engine/Renderables/SpriteRenderable_PixelCollision.js
+++ src/Engine/Renderables/SpriteRenderable_PixelCollision.js
@@ -1,20 +1,4 @@
-/* File: SpriteRenderable_PixelCollision.js 
- *
- * implements the _setTexInfo() function to support per-pixel collision for 
- * sprite elements
- */
-
-/*jslint node: true, vars: true */
-/*global gEngine, SpriteRenderable */
-/* find out more about jslint: http://www.jslint.com/help.html */
-
-"use strict";  // Operate in Strict mode such that variables must be declared before used!
-
-/**
- * implements the _setTexInfo() function to support per-pixel collision for sprite elements
- * @memberOf SpriteRenderable
- * @returns {void}
- */
+"use strict"; 
 SpriteRenderable.prototype._setTexInfo = function () {
     var imageW = this.mTextureInfo.mWidth;
     var imageH = this.mTextureInfo.mHeight;
@@ -25,6 +9,3 @@ SpriteRenderable.prototype._setTexInfo = function () {
     this.mTexWidth = ((this.mTexRight - this.mTexLeft) * imageW) + 1;
     this.mTexHeight = ((this.mTexTop - this.mTexBottom) * imageH) + 1;
 };
-//--- end of Public Methods
-//
-//</editor-fold>
