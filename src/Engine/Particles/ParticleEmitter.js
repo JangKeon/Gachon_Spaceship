15163ce61619ea21f4ab012075a3f12b89532d95
SUPERKEON\keon <up2sky7@naver.com>
2021-11-12 23:36:31 +0900

    ParticleSystem

--- src/Engine/Particles/ParticleEmitter.js
+++ src/Engine/Particles/ParticleEmitter.js
@@ -1,24 +1,5 @@
-/* 
- * File: ParticleEmitter.js
- * 
- */
-
-/*jslint node: true, vars:true , white: true*/
-/*global gEngine, vec2, ParticleGameObject */
-/* find out more about jslint: http://www.jslint.com/help.html */
-
 "use strict";
 
-/**
- * Default Constructor<p>
- * 
- * @param {vec2} pos Position of Emitter
- * @param {Number} num Number of particles to be emitted
- * @param {function} createrFunc Creater Function
- * @returns {ParticleEmitter} New instance of ParticleEmitter
- * @class ParticleEmitter
- * @type ParticleEmitter
- */
 function ParticleEmitter(pos, num, createrFunc) {
     // Smallest number of particle emitted per cycle
     this.kMinToEmit = 5;
@@ -31,17 +12,9 @@ function ParticleEmitter(pos, num, createrFunc) {
     
     this.mParticleCreator = createrFunc;
 }
-/**
- * Checks to see if the emitter has spawnned all of the 
- * @returns {Boolean} Whether all the particles has ben emitted
- * @memberOf ParticleEmitter
- */
+
 ParticleEmitter.prototype.expired = function () { return (this.mNumRemains <= 0); };
-/**
- * Emits particles and adds them to the particle set being passed
- * @param {ParticleGameObjectSet} pSet The ParticleGameObjectSet to add the particles to
- * @memberOf ParticleEmitter
- */
+
 ParticleEmitter.prototype.emitParticles = function (pSet) {
     var numToEmit = 0;
     if (this.mNumRemains < this.kMinToEmit) {
